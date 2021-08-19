3943feb487b191922a8a5df82c56037aaa4f7164
Cauan Felipe Tavares <tavarescauanf@gmail.com>
2021-08-18 21:56:09 -0300

    Login Page resposiva apenas para desktop

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -5,7 +5,7 @@ class Input extends Component{
 
     render(){
         return(
-            <input className="m-5 p-3 border-black border-2"></input>
+            <input className="m-5 p-3 border-black border-2 w-80 self-center" placeholder={this.props.placeholder}></input>
         )
     }
 }
