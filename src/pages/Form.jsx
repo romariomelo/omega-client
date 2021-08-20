6cc059ad361579c39ff531c606d8df49d37a6240
Cauan Felipe Tavares <tavarescauanf@gmail.com>
2021-08-20 15:20:09 -0300

    Começo do formulario e refatorações

---
+++ src/pages/Form.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/header"
+
+function Form(){
+    return(
+        <>
+        <Header />
+        <h1 className="text-center text-3xl my-4">
+            Crie sua proposta
+        </h1>
+        </>
+    )
+}
+export default Form
